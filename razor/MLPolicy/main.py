73fe25a57953562f82bac606dc408b43bfcb74c1
Nham Le <nham.le@sri.com>
2019-06-10 15:52:30 -0700

    normalize data

--- razor/MLPolicy/main.py
+++ razor/MLPolicy/main.py
@@ -1,60 +1,53 @@
 import torch
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from utils import Dataset
+from net import Net
 import os
 
+from sklearn import preprocessing
 import torch.optim as optim
 DEBUG = False
 
-
 model_path = "/Users/e32851/workspace/OCCAM/razor/MLPolicy/model"
+
+#load latest model or create a new one
+if not os.path.exists(model_path):
+    print("No existing model. Create a new one.")
+    net = Net()
+    for params in net.parameters():
+        print(params)
+    torch.save(net, model_path)
+    example = torch.rand(1, 3)
+    traced_script_module = torch.jit.trace(net, example)
+
+    traced_script_module.save("model.pt")
+    quit()
+else:
+    print("Found an existing model. Load %s"%model_path)
+    net = torch.load(model_path)
 
 dataset = Dataset("/Users/e32851/workspace/OCCAM/examples/portfolio/tree/slash/", no_of_feats = 3).all_data[0]
 print(dataset["input"][:4])
 print(dataset["output"][:4])
 print("best score for this batch: ", dataset["score"])
-X = torch.FloatTensor(dataset["input"])
-Y = torch.FloatTensor(dataset["output"])
-print(X.size())
-print(Y.size())
+X = dataset["input"]
+#normalize input
+min_max_scaler = preprocessing.MinMaxScaler()
+X = min_max_scaler.fit_transform(X)
 
-class Net(nn.Module):
+X = torch.FloatTensor(X)
+print(X)
+Y_target = torch.FloatTensor(dataset["output"])
 
-    def __init__(self):
-        super(Net, self).__init__()
-        self.fc1 = nn.Linear(3, 3)  # 6*6 from image dimension 
-        self.fc2 = nn.Linear(3, 2)
-        
+#trial run
+Y_pred = net.forward(X)
+print("trial running: \n", Y_pred)
 
-    def forward(self, x):
-        x = x.view(-1, self.num_flat_features(x))
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.softmax(x, dim = 1)
-        return x
-
-    def num_flat_features(self, x):
-        size = x.size()[1:]  # all dimensions except the batch dimension
-        num_features = 1
-        for s in size:
-            num_features *= s
-        return num_features
-
-#load latest model
-if not os.path.exists(model_path):
-    print("No existing model. Create a new one.")
-    net = Net()
-else:
-    print("Found an existing model. Load %s"%model_path)
-    net = torch.load(model_path)
-
-Y_target = Y
 
 criterion = nn.MSELoss()
 # create your optimizer
-optimizer = optim.SGD(net.parameters(), lr=0.01)
+optimizer = optim.Adam(net.parameters(), lr=0.001)
 
 for i in range(400):
 # in your training loop:
@@ -69,6 +62,12 @@ for i in range(400):
                 print(params)
     loss.backward()
     optimizer.step()    # Does the update
+
+
+#print out the model for debugging purpose
+print("Final model")
+for params in net.parameters():
+    print(params)
 torch.save(net, model_path)
 example = torch.rand(1, 3)
 traced_script_module = torch.jit.trace(net, example)
