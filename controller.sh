0deb24abefe6b04b79021ae73ff77ad26b40f84d
Nham Le <nham.le@sri.com>
2019-06-10 15:18:22 -0700

    fix folder path in build and the run script

--- controller.sh
+++ controller.sh
@@ -1,25 +1,44 @@
 #!/usr/bin/env bash
+
 NO_OF_ITERATION=1
 NO_OF_SAMPLING=2
 WORKDIR="/Users/e32851/workspace/OCCAM/examples/portfolio/tree/"
-while getopts iter:sampling:workdir option
+CURRENT_DIR=${PWD}
+while [[ $# -gt 0 ]]
 do
-    case "${option}"
-    in
-        iter) NO_OF_ITERATION=${OPTARG};;
-        sampling) NO_OF_SAMPLING=${OPTARG};;
-        workdir) WORKDIR=${OPTARG};;
+    key="$1"
+    case $key in
+        -i|--iter)
+	          NO_OF_ITERATION="$2"
+	          shift # past argument
+	          shift # past value
+	          ;;
+        -folder|--folder)
+	          WORKDIR="$2"
+	          shift # past argument
+	          shift # past value
+            ;;
+        -s|--sampling)
+            NO_OF_SAMPLING="$2"
+	          shift # past argument
+	          shift # past value
+	          ;;
     esac
 done
+
 echo Running training with $NO_OF_ITERATION iterations, $NO_OF_SAMPLING sampling each iteration
 echo at $WORKDIR
 
 #bootstrap
 python3 /Users/e32851/workspace/OCCAM/razor/MLPolicy/main.py
 
-for i in 0 .. $NO_OF_ITERATION
+cd $WORKDIR
+
+for i in $( seq 0 $NO_OF_ITERATION)
 do
          echo Iteration $i
-         (cd $WORKDIR ;   seq -w 0 $NO_OF_SAMPLING | parallel ./build.sh -folder {})
+         seq -w -s "_${i}\n" 0 $NO_OF_SAMPLING | parallel ./build.sh -folder {}
          python3 /Users/e32851/workspace/OCCAM/razor/MLPolicy/main.py
 done
+
+cd $CURRENT_DIR
