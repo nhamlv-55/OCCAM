afea03c5202a737a61bfd1dfebe0a609b6316e7d
root <nham.le@sri.com>
2019-07-05 22:37:07 +0000

    implement push_to_memory in Dataset

--- razor/MLPolicy/utils.py
+++ razor/MLPolicy/utils.py
@@ -6,17 +6,41 @@ import numpy as np
 import torch
 from sklearn.model_selection import train_test_split
 
-class Dataset:
+from collections import namedtuple
+
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+class ReplayMemory(object):
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
+
+    def push(self, *args):
+        """Saves a transition."""
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = Transition(*args)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+class Dataset(object):
     def __init__(self, folder, n_unused_stat = 3, size=-1):
         self.folder = folder
+        print(self.folder)
         self.n_unused_stat = n_unused_stat
         self.all_data = []
-        self.gamma = 0.99
         self.collect(size)
     def merge_csv(self, csv_files):
         episode_data = []
         raw_data  = []
-
+        print(csv_files)
         input = []
         output = []
         total = 0
@@ -58,18 +82,70 @@ class Dataset:
         sorted(runs)
         for r in runs[:size]:
             run_data = {}
-            #print(r)
+            print(r)
             csv_files = glob.glob(r+"/*.csv")
             _, episode_data, total = self.merge_csv(csv_files)
             result = self.get_stat(r)
             run_data["episode_data"] = episode_data
             run_data["score"] = self.score(result)
-            run_data["discounted_r"] = self.discounted_rewards(run_data["score"])
+            #run_data["discounted_r"] = self.discounted_rewards(run_data["score"])
             run_data["raw_result"] = result
             run_data["total"] = total
             self.all_data.append(run_data)
         #self.all_data.sort(key=lambda x: x["score"])
 
+    def push_to_memory(self, memory):
+
+        ##################################################
+        # action = select_action(state)                  #
+        # _, reward, done, _ = env.step(action.item())   #
+        # reward = torch.tensor([reward], device=device) #
+        #                                                #
+        # # Observe new state                            #
+        # last_screen = current_screen                   #
+        # current_screen = get_screen()                  #
+        # if not done:                                   #
+        #     next_state = current_screen - last_screen  #
+        # else:                                          #
+        #     next_state = None                          #
+        #                                                #
+        # # Store the transition in memory               #
+        # memory.push(state, action, next_state, reward) #
+        ##################################################
+
+        for eps in self.all_data:
+            for i in range(len(eps["episode_data"])):
+                print("run %s"%str(i))
+                sub_episode = eps["episode_data"][i]
+                if i==len(eps["episode_data"])-1:
+                    next_sub_episode = (None, None, None)
+                else:
+                    next_sub_episode = eps["episode_data"][i+1]
+                for j in range(len(sub_episode)):
+                    step = sub_episode[j]
+                    #print(step)
+                    if j==len(sub_episode)-1:
+                        next_step = next_sub_episode[0]
+                        #next_level = i+1
+                    else:
+                        next_step = sub_episode[j+1]
+                        #next_level = i
+                    state = step[0][:14]
+                    #state.append(i)
+                    action = step[1]
+                    if i==len(eps["episode_data"])-1 and j==len(sub_episode)-1:
+                        reward = eps["score"]
+                    else:
+                        reward = 0
+                    if next_step is not None:
+                        next_state = next_step[0][:14]
+                        #next_state.append(next_level)
+                    else:
+                        next_state = None
+                    if reward!=0:
+                        print(state, action, next_state, reward, "<<<")
+                    memory.push(state, action, next_state, reward)
+        
 
     def discounted_rewards(self, score):
         r = [0]*21
@@ -156,9 +232,12 @@ class Dataset:
                 print("------")
         print("best score", self.all_data[0]["score"])
         print("worst score", self.all_data[-1]["score"])
-        print("discounted r all_data[0]", self.all_data[0]["discounted_r"])
 if __name__== "__main__":
     OCCAM_HOME = os.environ['OCCAM_HOME']
     datapath = os.path.join(OCCAM_HOME, "examples/portfolio/tree/slash") 
-    dataset = Dataset(datapath, n_unused_stat = 3, size = 4)
+    dataset = Dataset(datapath, n_unused_stat = 3, size = 1)
+    memory = ReplayMemory(1000)
     dataset.dump()
+    dataset.push_to_memory(memory)
+    print("len memory:", len(memory.memory))
+    print(memory.memory[22])
