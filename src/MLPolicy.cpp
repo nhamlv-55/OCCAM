8b4550e0d00674729dd0a23293d43e7da12e58fe
root <nham.le@sri.com>
2019-07-06 02:18:09 +0000

    comment out debug printing

--- src/MLPolicy.cpp
+++ src/MLPolicy.cpp
@@ -188,7 +188,8 @@ namespace previrt {
     std::cerr<<"EPSILON:"<<epsilon<<std::endl;
     //s->append("TOUCH A CALL SITE\n");
     const bool explore = true;
-    const int type = 0; //Policy gradient
+    //const int type = 0; //Policy gradient
+    const int type = 1; // DQN
     llvm::Function *callee = CS.getCalledFunction();
     llvm::Function *caller = CS.getCaller();
     float q_Yes = -1;
