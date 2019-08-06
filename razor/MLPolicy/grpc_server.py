6d89e8c8b58c062d246e77932fc008b2773a81c8
root <nham.le@sri.com>
2019-08-06 17:17:04 +0000

    add grpc_mode to PolicyGradient

--- razor/MLPolicy/grpc_server.py
+++ razor/MLPolicy/grpc_server.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """The Python implementation of the gRPC route guide server."""
-
+from __future__ import print_function
 from concurrent import futures
 import time
 import math
@@ -22,30 +22,77 @@ import grpc
 
 import Previrt_pb2
 import Previrt_pb2_grpc
-
+import numpy as np
+import random
+import torch
+from utils import *
+from net import * 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
-_INTERACTIVE = True
+_INTERACTIVE = False
 class QueryOracleServicer(Previrt_pb2_grpc.QueryOracleServicer):
     """Provides methods that implement functionality of route guide server."""
 
-    def __init__(self):
-        pass
+    def __init__(self, net = None):
+        self.names = [
+            "block_count",
+            "inst_count",
+            "load_inst_count",
+            "store_inst_count",
+            "call_inst_count",
+            "branch_inst_count",
+            "loop_count",
+            "no_of_const",
+            "no_of_args",
+            "M_no_of_funcs",
+            "M_no_of_insts",
+            "M_no_of_blocks",
+            "M_no_of_direct_calls",
+            "callee_no_of_use",
+            "caller_no_of_use"
+        ]
+        self.net = net
+        #self.policy = policy_type(workdir, model_path, FeedForwardSingleInputSoftmax)
+        #self.policy.load(model_path)
+
+    def print_state(self, request):
+        features = request.features
+        trace = np.array(request.trace)
+        trace = trace.reshape(-1, len(self.names))
+        features = [int(s) for s in features.split(',')]
+        meta = request.meta
+        print("trace:")
+        print(trace)
+        print("meta:")
+        print(meta)
+        print("state:")
+        for i in range(len(features)):
+            print(self.names[i], ":", features[i])
 
     def Query(self, request, context):
-        print(request)
         if _INTERACTIVE:
+            self.print_state(request)
             pred = raw_input("Should I specialize?")
-            if pred=="y":
+            if pred.strip()=="y":
                 pred = True
             else:
                 pred = False
         else:
-            pass
+            self.print_state(request)
+            features = [int(s) for s in request.features.split(',')]
+            features = torch.FloatTensor([features])
+            #print(features.shape)
+            #print(self.net)
+            logits = self.net.forward(features).view(-1).detach().numpy()
+            print(logits)
+            pred = np.random.choice([False, True], p=logits)
+            
+            print(pred)
         context.set_trailing_metadata((
             ('blahdata', b'I agree'),
             ('retry', 'false'),
         ))
         return Previrt_pb2.Prediction(pred=pred)
+
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
     Previrt_pb2_grpc.add_QueryOracleServicer_to_server(
@@ -62,3 +109,10 @@ def serve():
 if __name__ == '__main__':
     logging.basicConfig()
     serve()
+
+##############################
+# server = ...               #
+# server.start()             #
+# policy.run_policy(100, 10) #
+# server.stop()              #
+##############################
