20a45c2ff058499a4714593a89977aa69cbc526c
root <nham.le@sri.com>
2019-08-08 21:08:07 +0000

    Finally a tiny example that behaves like we want

--- examples/fib/build.sh
+++ examples/fib/build.sh
@@ -5,7 +5,7 @@ cat > manifest <<EOF
 , "binary"  : "main"
 , "modules"    : []
 , "native_libs" : []
-, "args"    : ["10"]
+, "constraints"    : [1, "main", "a" ]
 , "name"    : "main"
 }
 EOF
@@ -13,18 +13,83 @@ EOF
 #make the bitcode
 CC=gclang make
 get-bc main
+#default values
+INTER_SPEC="none"
+INTRA_SPEC="machine-learning"
+DEVIRT="none"
+OPT_OPTIONS=""
+EPSILON="-10"
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    case $key in
+        -inter-spec|--inter-spec)
+            INTER_SPEC="$2"
+            shift # past argument
+            shift # past value
+            ;;
+        -folder|--folder)
+            PREFIX="run$2"
+            shift # past argument
+            shift # past value
+            ;;
+        -epsilon|--epsilon)
+            EPSILON="$2"
+            shift
+            shift
+            ;;
+        -intra-spec|--intra-spec)
+            INTRA_SPEC="$2"
+            shift # past argument
+            shift # past value
+            ;;
+        -disable-inlining|--disable-inlining)
+            OPT_OPTIONS="${OPT_OPTIONS} --disable-inlining"
+            shift # past argument
+            ;;
+        -ipdse|--ipdse)
+            OPT_OPTIONS="${OPT_OPTIONS} --ipdse"
+            shift # past argument
+            ;;
+        -ai-dce|--ai-dce)
+            OPT_OPTIONS="${OPT_OPTIONS} --ai-dce"
+            shift # past argument
+            ;;
+        -devirt|--devirt)
+            DEVIRT="$2"
+            shift # past argument
+            shift # past value
+            ;;    
+        -help|--help)
+            usage
+            exit 0
+            ;;
+        *)    # unknown option
+            POSITIONAL+=("$1") # save it in an array for later
+            shift # past argument
+            ;;
+    esac
+done
 
 
 export OCCAM_LOGLEVEL=INFO
-export OCCAM_LOGFILE=${PWD}/slash/occam.log
+export OCCAM_LOGFILE=${PWD}/slash/$PREFIX/occam.log
 
-slash --no-strip --intra-spec-policy=aggressive --inter-spec-policy=none --work-dir=slash manifest
+rm -rf slash/$PREFIX
+mkdir -p slash/$PREFIX
+#slash --no-strip --intra-spec-policy=aggressive --inter-spec-policy=none --stats --work-dir=slash/agg manifest 
 
-cp slash/main main_slash
+SLASH_OPTS="--inter-spec-policy=${INTER_SPEC} --intra-spec-policy=${INTRA_SPEC} --devirt=${DEVIRT} --no-strip --stats $OPT_OPTIONS --database=${DATABASE} --epsilon=$EPSILON"
+echo "============================================================"
+echo "Running with options ${SLASH_OPTS}"
+echo "============================================================"
+slash ${SLASH_OPTS} --work-dir=slash/$PREFIX manifest
 
 #debugging stuff below:
-for bitcode in slash/*.bc; do
-    ${LLVM_HOME}/bin/llvm-dis  "$bitcode" &> /dev/null
+for bitcode in slash/$PREFIX/*.bc; do
+    echo "$bitcode"
+    ${LLVM_HOME}/bin/llvm-dis  "$bitcode" -o "$bitcode".hr.bc
 done
 
 exit 0
---
+++ examples/fib/oldmain.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int fibo(int x) {
+  if (x <= 1) {
+    return x;
+  } else {
+    return fibo(x-1) + fibo(x-2);
+  }
+}
+
+int main(int argc, char* argv[]){
+  if (argc == 2) {
+    // Currently, user-input specialization does not understand
+    // library calls such as atoi.
+    //int n = atoi(argv[1]);
+    int res = fibo(argv[1][0]);
+    printf("Fibonacci of %d is %d\n",res, res);
+  }
+  
+  return 0;
+}
