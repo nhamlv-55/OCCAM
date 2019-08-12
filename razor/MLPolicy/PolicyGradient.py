fix reward calculation

razor/MLPolicy/PolicyGradient.py
@@ -17,7 +17,7 @@ import grpc
 import Previrt_pb2
 import Previrt_pb2_grpc
 from grpc_server import QueryOracleServicer
-DEBUG = False
+debug_print_limit = 6
 class PolicyGradient(BasePolicy):
     def __init__(self, workdir, model_path, network_type, network_hp, grpc_mode = False, debug = False):
         BasePolicy.__init__(self, workdir, model_path, network_type, network_hp, grpc_mode, debug)
@@ -42,7 +42,7 @@ class PolicyGradient(BasePolicy):
             if self.grpc_mode:
                 server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
                 Previrt_pb2_grpc.add_QueryOracleServicer_to_server(
-                    QueryOracleServicer(self.net), server)
+                    QueryOracleServicer(self.net, debug = False), server)
                 server.add_insecure_port('[::]:50051')
                 server.start()
                 self.run_policy(no_of_sampling, eps_threshold)
@@ -66,19 +66,20 @@ class PolicyGradient(BasePolicy):
 
         print(state_tensor.shape, reward_tensor.shape, action_tensor.shape)
         # Calculate loss
-        logprob = torch.log(
-            self.net.forward(state_tensor)).view(-1, 2)
-        if self.debug: print("state_tensor:", state_tensor)
-        if self.debug: print("logprob:", logprob)
-        if self.debug: print("reward_tensor", reward_tensor)
-        if self.debug: print("action_tensor", action_tensor)
+        predict_tensor = self.net.forward(state_tensor)
+        logprob = torch.log(predict_tensor).view(-1,2)
+        if self.debug: print("state_tensor:", state_tensor[:debug_print_limit])
+        if self.debug: print("predict_tensor:", predict_tensor[:debug_print_limit])
+        #if self.debug: print("logprob:", logprob)
+        if self.debug: print("reward_tensor", reward_tensor[:debug_print_limit])
+        if self.debug: print("action_tensor", action_tensor[:debug_print_limit])
         adv = logprob[np.arange(len(action_tensor)), action_tensor]
 
         if self.debug:
-            print("adv", adv, adv.shape)
+            print("adv", adv[:debug_print_limit], adv.shape)
         selected_logprobs = reward_tensor * adv
-        if self.debug: print("selected_logprobs", selected_logprobs)
-        loss = -selected_logprobs.mean()
+        if self.debug: print("selected_logprobs", selected_logprobs[:debug_print_limit])
+        loss = selected_logprobs.mean()
         print("loss at iteration %s:"%iteration, loss)
         # Calculate gradients
         loss.backward()

razor/MLPolicy/utils.py
@@ -133,7 +133,6 @@ class Dataset(object):
             self.raw_data.extend(_)
             run_data["episode_data"] = episode_data
             run_data["score"] = self.score(result)
-            #run_data["discounted_r"] = self.discounted_rewards(run_data["score"])
             run_data["raw_result"] = result
             run_data["total"] = total
             self.all_data.append(run_data)
@@ -161,8 +160,10 @@ class Dataset(object):
             batch_states.extend(states)
             batch_actions.extend(actions)
             batch_probs.extend(probs)
-            rewards = [eps["score"]]*len(states)
-            batch_rewards.extend(discounted_rewards(rewards, GAMMA))
+            #rewards = [eps["score"]]*len(states)
+            rewards = [0]*len(states)
+            rewards[-1] = eps["score"]
+            batch_rewards.extend(discount_rewards(rewards, GAMMA))
         return batch_states, batch_actions, batch_rewards, batch_probs 
     def push_to_memory(self, memory):
 
@@ -230,18 +231,16 @@ class Dataset(object):
         #        print("------")
         print("best score", self.all_data[0]["score"])
         print("worst score", self.all_data[-1]["score"])
-def discounted_rewards(rewards, gamma):
-    r = np.array([gamma**i * rewards[i] 
-                  for i in range(len(rewards))])
-    if DEBUG: print("r:", r)
-    # Reverse the array direction for cumsum and then
-    # revert back to the original order
-    r = r[::-1].cumsum()[::-1]
-    if DEBUG: print("r cumsum, mean, std:", r)
-    #r -= np.mean(r)
-    #r /= np.std(r)
-    #if DEBUG: print("scaled_dr:", r)
-    return r*100
+
+def discount_rewards(rewards, gamma):
+    discounted_rewards = np.zeros(len(rewards))
+    cumulative_rewards = 0
+    for i in reversed(range(0, len(rewards))):
+        cumulative_rewards = cumulative_rewards * gamma + rewards[i]
+        discounted_rewards[i] = cumulative_rewards
+    discounted_rewards -= np.mean(discounted_rewards)
+    discounted_rewards /= np.std(discounted_rewards)
+    return discounted_rewards
 def gen_new_meta(workdir, bootstrap_runs, run_command):
     dataset_path = os.path.join(workdir, "slash")
     metadata = {}