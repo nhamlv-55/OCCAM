a2af4af2fbbf2ce1c88d27bb8bf5a45c0014dca4
root <nham.le@sri.com>
2019-08-14 23:32:36 +0000

    add timer and switch to learn bigger or smaller score

--- razor/MLPolicy/PolicyGradient.py
+++ razor/MLPolicy/PolicyGradient.py
@@ -22,7 +22,8 @@ torch.manual_seed(0)
 np.random.seed(0)
 
 debug_print_limit = 6
-lr = 0.1
+lr = 0.01
+minimize = False
 class PolicyGradient(BasePolicy):
     def __init__(self, workdir, model_path, network_type, network_hp, grpc_mode = False, debug = False):
         BasePolicy.__init__(self, workdir, model_path, network_type, network_hp, grpc_mode, debug)
@@ -40,7 +41,8 @@ class PolicyGradient(BasePolicy):
         self.optimizer = optim.Adam(self.net.parameters(), lr = lr)
 
         for i in range(no_of_iter):
-            if (i+1)%10 == 0:
+            start_time = time.time()
+            if (i+1)%4 == 0:
                 print("performance at iteration %s"%str(i))
                 self.evaluate(tag="eval%s"%str(i))
             eps_threshold = -1 #set to -1 to always use policy
@@ -54,10 +56,18 @@ class PolicyGradient(BasePolicy):
                 server.stop(0)
             else:
                 self.run_policy(no_of_sampling, eps_threshold)
+            run_policy_time = time.time()
+            print("Rollout %s runs in %s seconds"%(no_of_sampling, run_policy_time - start_time))
             dataset = Dataset(self.dataset_path, size = no_of_sampling)
             trajectory_data = dataset.get_trajectory_data(normalize_rewards = True)
+            collect_data_time = time.time()
+            print("Processing data in ", collect_data_time - run_policy_time)
             self.optimize(trajectory_data, i)
-            self.save_model(model_path)
+            optimize_time = time.time()
+            print("Optimize in ", optimize_time  - collect_data_time)
+            if not self.grpc_mode:
+                self.save_model(model_path)
+            
 
     def optimize(self, trajectory_data, iteration):
         batch_states = trajectory_data[0]
@@ -84,19 +94,22 @@ class PolicyGradient(BasePolicy):
             print("adv", adv[:debug_print_limit], adv.shape)
         selected_logprobs = reward_tensor * adv
         if self.debug: print("selected_logprobs", selected_logprobs[:debug_print_limit])
-        loss = selected_logprobs.mean()
+        if minimize:
+            loss = selected_logprobs.mean()
+        else:
+            loss = -selected_logprobs.mean()
         print("loss at iteration %s:"%iteration, loss)
         # Calculate gradients
         loss.backward()
-        print("before updating parameters-------------")
         if self.debug:
+            print("before updating parameters-------------")
             for param in self.net.parameters():
                 print("params:", param.data.view(1, -1),)
         
         # Apply gradients
         self.optimizer.step()
-        print("after updating parameters---------------")
         if self.debug:
+            print("after updating parameters---------------")
             for param in self.net.parameters():
                 print("grads:", param.grad.view(1, -1))
             for param in self.net.parameters():
