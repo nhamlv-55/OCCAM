6df706dbf6f1032de717ce3d891348094b2254bc
Nham Le <nham.le@sri.com>
2019-08-15 18:01:20 +0000

    fix build for bzip2

--- examples/portfolio/curl/build.sh
+++ examples/portfolio/curl/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #make sure we have these libs to link
-apt install libssl-dev
+#apt install libssl-dev
 # Make sure we exit if there is a failure
 set -e
 
@@ -9,65 +9,72 @@ function usage() {
     echo "       VAL1=none|dsa|cha_dsa"    
     echo "       VAL2=none|aggressive|nonrec-aggressive"
 }
-
+#make the bitcode
 #default values
 INTER_SPEC="none"
 INTRA_SPEC="machine-learning"
 DEVIRT="none"
 OPT_OPTIONS=""
-
+EPSILON="-10"
 POSITIONAL=()
 while [[ $# -gt 0 ]]
 do
-key="$1"
-case $key in
-    -inter-spec|--inter-spec)
-	INTER_SPEC="$2"
-	shift # past argument
-	shift # past value
-	;;
-    -intra-spec|--intra-spec)
-	INTRA_SPEC="$2"
-	shift # past argument
-	shift # past value
-	;;
-    -folder|--folder)
-	      PREFIX="run$2"
-	      shift # past argument
-	      shift # past value
-        ;;
-    -epsilon|--epsilon)
-        EPSILON="$2"
-        shift
-        shift
-        ;;
--disable-inlining|--disable-inlining)
-	OPT_OPTIONS="${OPT_OPTIONS} --disable-inlining"
-	shift # past argument
-	;;
-    -ipdse|--ipdse)
-	OPT_OPTIONS="${OPT_OPTIONS} --ipdse"
-	shift # past argument
-	;;
-    -ai-dce|--ai-dce)
-	OPT_OPTIONS="${OPT_OPTIONS} --ai-dce"
-	shift # past argument
-	;;        
-    -devirt|--devirt)
-	DEVIRT="$2"
-	shift # past argument
-	shift # past value
-	;;        
-    -help|--help)
-	usage
-	exit 0
-	;;
-    *)    # unknown option
-	POSITIONAL+=("$1") # save it in an array for later
-	shift # past argument
-	;;
-esac
+    key="$1"
+    case $key in
+        -inter-spec|--inter-spec)
+            INTER_SPEC="$2"
+            shift # past argument
+            shift # past value
+            ;;
+        -folder|--folder)
+            PREFIX="run$2"
+            shift # past argument
+            shift # past value
+            ;;
+        -epsilon|--epsilon)
+            EPSILON="$2"
+            shift
+            shift
+            ;;
+        -intra-spec|--intra-spec)
+            INTRA_SPEC="$2"
+            shift # past argument
+            shift # past value
+            ;;
+        -disable-inlining|--disable-inlining)
+            OPT_OPTIONS="${OPT_OPTIONS} --disable-inlining"
+            shift # past argument
+            ;;
+        -ipdse|--ipdse)
+            OPT_OPTIONS="${OPT_OPTIONS} --ipdse"
+            shift # past argument
+            ;;
+        -ai-dce|--ai-dce)
+            OPT_OPTIONS="${OPT_OPTIONS} --ai-dce"
+            shift # past argument
+            ;;
+        -g|--grpc)
+            OPT_OPTIONS="${OPT_OPTIONS} --grpc"
+            shift # past argument
+            ;;
+        -devirt|--devirt)
+            DEVIRT="$2"
+            shift # past argument
+            shift # past value
+            ;;    
+        -help|--help)
+            usage
+            exit 0
+            ;;
+        *)    # unknown option
+            POSITIONAL+=("$1") # save it in an array for later
+            shift # past argument
+            ;;
+    esac
 done
+
+
+
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
