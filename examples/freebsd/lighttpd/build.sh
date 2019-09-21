4a22cf2b367e3bf283d74b13ce262a738e2495a2
Nham Le <nham.le@sri.com>
2019-09-21 15:20:57 +0000

    add build for lighttpd

---
+++ examples/freebsd/lighttpd/build.sh
@@ -0,0 +1,124 @@
+#!/usr/bin/env bash
+# Prereqs: Make sure OCCAM envirnment is setup and WLLVM is installed
+# env vars should be setup
+
+# set CC to clang. Dragonegg works too
+export LLVM_COMPILER=clang
+export WLLVM_OUTPUT=WARNING
+
+WORKING_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+INSTALL_DIR=${WORKING_DIR}/install
+
+if [ -d "$INSTALL_DIR" ];
+then
+    echo "Install directory already exists. Skipped compilation ..."
+else
+    # download and fetch source
+    wget http://lighttpd.net/download/lighttpd-1.4.13.tar.gz
+    tar -zxf lighttpd-1.4.13.tar.gz
+    cd lighttpd-1.4.13
+    # configure without shared libs...will save headaches
+    CC=gclang ./configure --without-openssl --without-pcre --without-zlib --without-bzip2 --prefix=${WORKING_DIR}/install
+    CC=gclang make
+    make install
+fi
+
+# Extract bitcode
+get-bc ${INSTALL_DIR}/sbin/lighttpd 
+mv ${INSTALL_DIR}/sbin/lighttpd.bc ${WORKING_DIR}
+
+## Run Occam
+
+cd ${WORKING_DIR}
+
+# set up manifests
+ cat > lhttpd.manifest <<EOF
+{ "main" : "lighttpd.bc"
+, "binary"  : "lighttpd"
+, "args"    : ["-D", "-m", "/", "-f", "myconf.conf"]
+, "name"    : "lighttpd"
+, "modules" : []
+, "ldflags" : ["-flat_namespace", "-undefined", "suppress", "-ldl"] 
+}
+
+EOF
+function usage() {
+    echo "Usage: $0 [--disable-inlining] [--ipdse] [--ai-dce] [--devirt VAL1] [--inter-spec VAL2] [--intra-spec VAL2] [--help]"
+    echo "       VAL1=none|dsa|cha_dsa"    
+    echo "       VAL2=none|aggressive|nonrec-aggressive"
+}
+INTER_SPEC="none"
+INTRA_SPEC="machine-learning"
+DEVIRT="none"
+OPT_OPTIONS=""
+EPSILON="-10"
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    case $key in
+        -inter-spec|--inter-spec)
+            INTER_SPEC="$2"
+            shift # past argument
+            shift # past value
+            ;;
+        -folder|--folder)
+            PREFIX="run$2"
+            shift # past argument
+            shift # past value
+            ;;
+        -epsilon|--epsilon)
+            EPSILON="$2"
+            shift
+            shift
+            ;;
+        -intra-spec|--intra-spec)
+            INTRA_SPEC="$2"
+            shift # past argument
+            shift # past value
+            ;;
+        -disable-inlining|--disable-inlining)
+            OPT_OPTIONS="${OPT_OPTIONS} --disable-inlining"
+            shift # past argument
+            ;;
+        -ipdse|--ipdse)
+            OPT_OPTIONS="${OPT_OPTIONS} --ipdse"
+            shift # past argument
+            ;;
+        -ai-dce|--ai-dce)
+            OPT_OPTIONS="${OPT_OPTIONS} --ai-dce"
+            shift # past argument
+            ;;
+        -g|--grpc)
+            OPT_OPTIONS="${OPT_OPTIONS} --grpc"
+            shift # past argument
+            ;;
+        -devirt|--devirt)
+            DEVIRT="$2"
+            shift # past argument
+            shift # past value
+            ;;    
+        -help|--help)
+            usage
+            exit 0
+            ;;
+        *)    # unknown option
+            POSITIONAL+=("$1") # save it in an array for later
+            shift # past argument
+            ;;
+    esac
+done
+
+DATABASE=${PWD}/slash/$PREFIX/
+export OCCAM_LOGLEVEL=INFO
+export OCCAM_LOGFILE=${PWD}/slash/$PREFIX/occam.log
+
+mkdir -p $DATABASE
+# OCCAM
+SLASH_OPTS="--inter-spec-policy=${INTER_SPEC} --intra-spec-policy=${INTRA_SPEC} --devirt=${DEVIRT} --no-strip --stats $OPT_OPTIONS --database=${DATABASE} --epsilon=$EPSILON"
+echo "============================================================"
+echo "Running with options ${SLASH_OPTS}"
+echo "============================================================"
+slash ${SLASH_OPTS} --work-dir=slash/$PREFIX lhttpd.manifest
+
+python ${OCCAM_HOME}/razor/MLPolicy/GSA_util/GSA.py -r $PREFIX -f ${PWD} 
