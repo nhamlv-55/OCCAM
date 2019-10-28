a80c1578627fbbaef5bc4b1880afc8bdf522a5cc
Nham Le <nham.le@sri.com>
2019-10-28 17:45:46 +0000

    dump RL loop to test implementation

--- razor/MLPolicy/main_baseline.py
+++ razor/MLPolicy/main_baseline.py
@@ -6,3 +6,7 @@ env = OccamGymEnv(workdir = os.path.join(OCCAM_HOME, "examples/portfolio/tree"),
                   mode = Mode.TRAINING,
                   idx = "2",
                   connection = 'localhost:50051')
+
+while True:
+    action = True
+    obs, rewards, dones, info = env.step(action)
